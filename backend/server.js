13e8cce56ea012b78ae6eda821e4603a7b121917
Mahmudul Ferdous <107098783+2020331037@users.noreply.github.com>
2025-06-04 11:28:29 +0600

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const vlogRoutes = require('./routes/vlogRoutes');
+
+const app = express();
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+app.use('/api/vlogs', vlogRoutes);
+
+mongoose.connect('mongodb://localhost:27017/jobportal', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
