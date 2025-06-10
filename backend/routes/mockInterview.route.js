25cb754bdcf5084dd6b5f235eaae11e87e44d353
Risan85 <106927714+Risan85@users.noreply.github.com>
2025-06-10 13:00:39 +0600

    Update mockInterview.route.js

--- backend/routes/mockInterview.route.js
+++ backend/routes/mockInterview.route.js
@@ -4,5 +4,6 @@ import { startMockInterview } from "../controllers/mockInterview.controller.js";
 const router = express.Router();
 
 router.post("/", startMockInterview);
+router.post("/submit", submitMockInterview);
 
 export default router;
