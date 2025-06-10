8fe586aa7776a4e431f52a412ab0e07f70b2d87d
Risan85 <106927714+Risan85@users.noreply.github.com>
2025-06-10 12:05:40 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 import interviewToolkitRoute from "./routes/interviewToolkit.route.js";
+import mockInterviewRoute from "./routes/mockInterview.route.js";
 
 
 dotenv.config({});
